126f07c5c197040859af66d9b23005a9da5d2166
JesusL1 <JesusL1@users.noreply.github.com>
2021-03-08 14:01:59 -0800

    Add loading spinner

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import SearchResult from './components/SearchResult'
-import {Form, Button} from 'react-bootstrap'
+import { Button, Form, FormControl, Nav, Navbar, Spinner } from 'react-bootstrap'
 
 
 const App = () => {
 
   const [ searchTerm, setSearchTerm ] = useState('')
+  const [ loading, setLoading ] = useState(false)
+  const [ success, setSuccess ] = useState(false)
   const [ productName, setProductName ] = useState('')
   const [ productPrice, setProductPrice ] = useState('')
   const [ productImg, setProductImg ] = useState('')
@@ -21,31 +23,58 @@ const App = () => {
 
   const search = (event) => {
     event.preventDefault()
-    axios.get(`http://localhost:3001/${encodeURIComponent(searchTerm)}`)
+    const request = axios.get(`http://localhost:3001/${encodeURIComponent(searchTerm)}`)
       .then(res => {
           const p = res.data
           console.log('Product: ', p)
-          setProductName(p.name)
-          setProductPrice(p.price)
-          setProductImg(p.img)
+          if (p.name.length>1 && p.price && p.img.length>1 ) {
+            setProductName(p.name)
+            setProductPrice(p.price)
+            setProductImg(p.img)
+            setSuccess(true)
+          }
+          setLoading(false)
       })
+      .catch(error => {
+        setLoading(false)
+        console.log(error)
+      })
+      setLoading(true)
+  }
+
+  const LoadingSpinner = () => {
+    if (loading) {
+      return (
+        <Spinner id="spinner" animation="border" variant="dark" size="lg" />
+      )
+    }
+    else  {return null}
   }
 
   return (
-    <div className="container">
-      <h1>PriceWatcher</h1>
-      <Form onSubmit={search}>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Nav className="mr-auto">
+          <Nav.Link href="#Login">Login</Nav.Link>
+          <Nav.Link href="#watchlist">Watchlist</Nav.Link>
+          <Nav.Link href="#account">Account</Nav.Link>
+        </Nav>
+        <Form onSubmit={search} inline>
+          <FormControl className="mr-sm-2" id="productInput" type="url" value={searchTerm} onChange={handleSearchChange} onFocus={handleFocus} placeholder="https://www.example.com/product-page"/>
+          <Button variant="outline-info" type="submit">Search</Button>
+        </Form>
+      </Navbar>
+
+      <LoadingSpinner/>
+      <SearchResult product={{name: productName, price: productPrice, img: productImg }} success={success} /> 
+      {/* <h1 className="ml4">PriceWatcher</h1> */}
+      {/* <Form onSubmit={search}>
         <Form.Group className="form-inline">
-          <Form.Control id="productInput" type="url" value={searchTerm} onChange={handleSearchChange} onFocus={handleFocus} placeholder="https://www.exampleStore.com/productPage" />
+          <Form.Control id="productInput" type="url" value={searchTerm} onChange={handleSearchChange} onFocus={handleFocus} placeholder="https://www.example.com/product-page" />
           <Button variant="primary" type="submit">Search</Button>
         </Form.Group>
-      </Form>
-      {/* <form onSubmit={search}>          
-        <input type="url" value={searchTerm} onChange={handleSearchChange} placeholder="Enter Product URL"/> 
-        <button type="submit">Search</button>
-      </form>  */}
-      <SearchResult product={{name: productName, price: productPrice, img: productImg }} />
-      <p>{searchTerm}</p>
+      </Form> */}
+      {/* <SearchResult product={{name: productName, price: productPrice, img: productImg }} /> */}
     </div>
   )
 }
--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -1,11 +1,17 @@
-const SearchResult = ({product}) => {
-    return (
-        <div>
-            <h3>{product.name}</h3>
-            <h3>${product.price}</h3>
-            <img src={product.img} alt='' width='430rem' height='430rem' />
-        </div>
-    )
+const SearchResult = ({product, success}) => {
+    if (success) {
+        return (
+            <div>
+                <h3>{product.name}</h3>
+                <h3>${product.price}</h3>
+                <img src={product.img} alt='' width='430rem' height='430rem' />
+            </div>
+        )
+    }
+    else {
+        return null
+    }
+   
 }
 
 export default SearchResult;
