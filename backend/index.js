7c295523db6fe18e10843043428a8660f211f93c
JesusL1 <JesusL1@users.noreply.github.com>
2021-03-07 22:02:40 -0800

    Update validity checking of a given url and response

--- backend/index.js
+++ backend/index.js
@@ -11,23 +11,32 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 app.get('/:url', (request, response) => {
-  console.log('request made it here')
-  const url = new URL(request.params.url)
-  
-  if (url.hostname in webScraper.stores) {
-    webScraper.selectParse(url)
-      .then(product => {
+  const product = webScraper.selectParse(request.params.url)
+    .then(product => {
         console.log('Product: ', product)
         response.json(product)
-      })
-  }
-  else {
-    console.log('INVALID WEBSITE')
-    response.send('INVALID WEBSITE')
-  }
+    })
+    .catch(error => {
+        console.log('app: ', error.message)
+        return response.status(400).json({ error: 'invalid product URL' })
+    })
 })
 
 
 app.listen(port, () => {
   console.log(`Server running on port http://localhost:${port}`)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'TypeError') {
+    return response.status(400).send({ error: 'invalid product URL' })
+  }
+  next(error)
+}
+app.use(errorHandler)
--- backend/web_scraper.js
+++ backend/web_scraper.js
@@ -4,45 +4,43 @@ const { JSDOM } = jsdom;
 
 const stores = {"93brand.com":"scrape_93Brand", "www.amazon.com": "scrape_Amazon", "bananarepublic.gap.com":"scrape_Bananarepublic", "www.microcenter.com":"scrape_Microcenter"}
 
-stores.scrape_Microcenter = (url) => {
+stores.scrape_Microcenter = (data) => {
     console.log('microcenter running')
-    const promise = loadDoc(url)
-        .then((dom) => {
-            const productName = dom.title
-            let productPrice = dom.getElementById('pricing').textContent.substring(1)
-            productPrice = parseFloat(productPrice.replace(/,/g, ''))
-            const productImg = dom.querySelectorAll('.productImageZoom')[0].getAttribute('src').replace('thumbnail', 'zoom') // gets a larger image
-            const productObject = {name: productName, price: productPrice, img: productImg}
-            return productObject
-        })
-        .catch(error=> {
-            console.log(error)
-        })
-        return promise
+    const dom = loadDoc(data)
+    const productName = dom.title
+    let productPrice = dom.getElementById('pricing').textContent.substring(1)
+    productPrice = parseFloat(productPrice.replace(/,/g, ''))
+    const productImg = dom.querySelectorAll('.productImageZoom')[0].getAttribute('src').replace('thumbnail', 'zoom') // gets a larger image
+    const productObject = {name: productName, price: productPrice, img: productImg}
+    return productObject
 }
 
 stores.scrape_93Brand = (url) => {
     console.log('scrape_93Brand function')
 }
 
-const selectParse = (url) => {
-    const scrape_function = stores[url.hostname]
-    console.log(scrape_function)
-    const product = stores[scrape_function](url.href)
-    return product
+const selectParse = (url_link) => {
+    const promise = axios.get(url_link)
+    .then((res) => {
+        const url = new URL(url_link)
+        if (url.hostname in stores) {
+            const scrape_function = stores[url.hostname]
+            console.log(scrape_function)
+            const product = stores[scrape_function](res.data)
+            return product
+        }
+    })
+    .catch(error=> {
+        //console.log('selectParse: ', error.message)
+        return Promise.reject(error)
+    })
+    return promise
 }
 
-const loadDoc = (url) => {
-    const promise = axios.get(url)
-        .then(res => {
-            const domPreParse = new JSDOM(res.data, { pretendToBeVisual: true })
-            const dom = domPreParse.window.document
-            return dom
-        })
-        .catch(error=> {
-            console.log(error)
-        })
-     return promise
+const loadDoc = (data) => {
+    const domPreParse = new JSDOM(data, { pretendToBeVisual: true })
+    const dom = domPreParse.window.document
+    return dom
 }
 
 
